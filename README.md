4768c18d8038ef5e0383a4b3f9ad9696d3d689f5
yaelramirez <yaleramso@gmail.com>
2025-02-05 00:49:01 -0600

    Segunda actualización

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tripleten web_project_around
+
+Este proyecto trata de una página interactiva en la que los usuarios pueden añadir, eliminar o dar "me gusta" a las fotos de los lugares más representativos de los Estados Unidos.
+
+Las técnicas y tecnologías usadas para este proyecto fueron los lenguajes de programación como HTML, JavaScript y CSS, además del uso de plataformas para desarrolladores como GitHub.
---
+++ scripts/index.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let buttonEdit = document.querySelector(".profile__edit");
+  let openPopup = document.querySelector(".popup");
+  let buttonClose = document.querySelector(".popup__close");
+  let closePopup = document.querySelector(".popup");
+  let buttonsLike = document.querySelectorAll(".element__button");
+  let inputName = document.querySelector(".form__input_name");
+  let inputAbout = document.querySelector(".form__input_about");
+  let textTitle = document.querySelector(".profile__title");
+  let textText = document.querySelector(".profile__text");
+  let textName = textTitle.textContent;
+  let textAbout = textText.textContent;
+  let buttonSubmit = document.querySelector(".form__submit");
+  const loaderTimeout = 5000;
+  function open() {
+    if (openPopup.style.display === "none" || openPopup.style.display === "") {
+      openPopup.style.display = "flex";
+      document.getElementById("loader").style.display = "block";
+      document.getElementById("loader__circle").style.display = "none";
+      setTimeout(function () {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("loader__circle").style.display = "block";
+        inputName.value = textName;
+        inputAbout.value = textAbout;
+        buttonSubmit.style.backgroundColor = "#000000";
+      }, loaderTimeout);
+    } else {
+      openPopup.style.display = "none";
+    }
+  }
+
+  function close() {
+    if (closePopup.style.display === "flex") {
+      closePopup.style.display = "none";
+      inputName.value = "";
+      inputAbout.value = "";
+      buttonSubmit.style.backgroundColor = "#FFFFFF";
+    } else {
+      closePopup.style.display = "flex";
+    }
+  }
+
+  function like() {
+    let button = event.target;
+    let currentBackground = window.getComputedStyle(button).backgroundImage;
+    if (currentBackground.includes("btn-like-nr.png")) {
+      button.style.backgroundImage = 'url("./images/btn-like-ac.png")';
+    } else {
+      button.style.backgroundImage = 'url("./images/btn-like-nr.png")';
+    }
+  }
+
+  function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(inputName.value);
+    console.log(inputAbout.value);
+    textTitle.textContent = inputName.value;
+    textText.textContent = inputAbout.value;
+    closePopup.style.display = "none";
+    inputName.value = "";
+    inputAbout.value = "";
+    buttonSubmit.style.backgroundColor = "#FFFFFF";
+  }
+
+  buttonClose.addEventListener("click", close);
+  buttonsLike.forEach((button) => {
+    button.addEventListener("click", like);
+  });
+  buttonSubmit.addEventListener("click", handleProfileFormSubmit);
+  buttonEdit.addEventListener("click", open);
+});
