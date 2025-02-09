73418b1ffdb50bd0b8f83e66e789669f92f858c0
yaelramirez <yaleramso@gmail.com>
2025-02-09 12:43:31 -0600

    tercera actualización

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   let buttonEdit = document.querySelector(".profile__edit-info");
+  let buttonAddProfile = document.querySelector(".profile__add-profile");
   let openPopup = document.querySelector(".popup");
   let buttonClose = document.querySelector(".popup__close");
   let closePopup = document.querySelector(".popup");
   let buttonsLike = document.querySelectorAll(".element__button");
   let inputName = document.querySelector(".form__input_name");
   let inputAbout = document.querySelector(".form__input_about");
-  let textTitle = document.querySelector(".profile__title");
-  let textText = document.querySelector(".profile__text");
+  let textTitle = document.querySelector(".profile__username");
+  let textText = document.querySelector(".profile__about-me");
   let buttonSubmit = document.querySelector(".form__submit");
   const loaderTimeout = 5000;
+
   function open() {
     if (openPopup.style.display === "none" || openPopup.style.display === "") {
       openPopup.style.display = "flex";
@@ -18,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
       setTimeout(function () {
         document.getElementById("loader").style.display = "none";
         document.getElementById("loader__circle").style.display = "block";
-        inputName.value = textName;
-        inputAbout.value = textAbout;
+        inputName.value = textTitle.textContent;
+        inputAbout.value = textText.textContent;
         buttonSubmit.style.backgroundColor = "#000000";
       }, loaderTimeout);
     } else {
@@ -38,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  function like() {
+  function like(event) {
     let button = event.target;
     let currentBackground = window.getComputedStyle(button).backgroundImage;
     if (currentBackground.includes("btn-like-nr.png")) {
@@ -50,16 +52,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function handleProfileFormSubmit(evt) {
     evt.preventDefault();
-    console.log(inputName.value);
-    console.log(inputAbout.value);
     textTitle.textContent = inputName.value;
     textText.textContent = inputAbout.value;
+    console.log(inputName.value);
+    console.log(inputAbout.value);
     closePopup.style.display = "none";
     inputName.value = "";
     inputAbout.value = "";
     buttonSubmit.style.backgroundColor = "#FFFFFF";
   }
 
+  buttonEdit.addEventListener("click", open);
+  buttonAddProfile.addEventListener("click", open);
   buttonClose.addEventListener("click", close);
   buttonsLike.forEach((button) => {
     button.addEventListener("click", like);
