2e02c3d2b45d24bebe3eef94e37a1256027737ae
yaelramirez11 <yaleramso@gmail.com>
2025-10-28 15:29:18 -0600

    Finalización de FormValidator.js

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,7 +1,82 @@
 class FormValidator {
-  constructor(formClassAndSelector, formElementToValidate) {
-    this._formClassAndSelector = formClassAndSelector;
+  constructor(formObjectSettings, formElementToValidate) {
+    this._formSelector = formObjectSettings.formSelector;
+    this._inputSelector = formObjectSettings.inputSelector;
+    this._submitButtonSelector = formObjectSettings.submitButtonSelector;
+    this._inactiveButtonStateClass =
+      formObjectSettings.inactiveButtonStateClass;
+    this._inputErrorClass = formObjectSettings.inputErrorClass;
+    this._inputErrorMessageClass = formObjectSettings.inputErrorMessageClass;
     this._formElementToValidate = formElementToValidate;
+    this._formObjectSettings = formObjectSettings;
+  }
+
+  _showInputError(inputElement, errorMessage) {
+    const errorElement = inputElement.nextElementSibling;
+    inputElement.classList.add(this._formObjectSettings.inputErrorClass); //Aplica una clase CSS que resalta el input como inválido (por ejemplo, borde rojo)
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._formObjectSettings.inputErrorMessageClass); //Aplica una clase CSS que hace visible el mensaje de error, si estaba oculto antes.
+  }
+
+  _hideInputError(inputElement) {
+    const errorElement = inputElement.nextElementSibling;
+    inputElement.classList.remove(this._formObjectSettings.inputErrorClass);
+    errorElement.classList.remove(
+      this._formObjectSettings.inputErrorMessageClass
+    );
+    errorElement.textContent = "";
+  }
+
+  _isValid(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => !inputElement.validity.valid);
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._buttonElement.classList.add(
+        this._formObjectSettings.inactiveButtonStateClass
+      );
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(
+        this._formObjectSettings.inactiveButtonStateClass
+      );
+      this._buttonElement.disabled = false;
+    }
+  }
+
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formElementToValidate.querySelectorAll(this._inputSelector)
+    );
+    this._buttonElement = this._formElementToValidate.querySelector(
+      this._submitButtonSelector
+    );
+
+    this._toggleButtonState();
+
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._isValid(inputElement);
+        this._toggleButtonState();
+      });
+    });
+
+    this._formElementToValidate.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+  }
+
+  enableValidation() {
+    this._setEventListeners();
   }
 }
 
--- scripts/validate.js
+++ scripts/validate.js
@@ -4,71 +4,13 @@ const settingsObject = {
   formSelector: ".form", //selector del formulario en específico (acerca de mi o nueva tarjeta) que queremos validar.
   inputSelector: ".form__input", //selector de cada campo de entrada específico de texto en los formularios(inputs), (nombre, URL, etc.)
   submitButtonSelector: ".form__submit", //selector del botón "submit" o "enviar" de cada formulario.
-  inactiveButtonStateClass: ".form__submit_inactive", //clase que habilita o deshabilita el botón de "enviar"
-  imputErrorClass: ".form__input_type_error", //clase que marca visualmente los campos que tienen O no tienen errores(p.e: borde rojo).
-  inputErrorMessageClass: ".form__input-error_active", //clase que muestra o esconde el mensaje de error debajo del input.
+  inactiveButtonStateClass: "form__submit_inactive", //clase que habilita o deshabilita el botón de "enviar"
+  inputErrorClass: "form__input_type_error", //clase que marca visualmente los campos que tienen O no tienen errores(p.e: borde rojo).
+  inputErrorMessageClass: "form__input-error_active", //clase que muestra o esconde el mensaje de error debajo del input.
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-  const showInputError = (inputElement, errorMessage) => {
-    const errorElement = inputElement.nextElementSibling;
-    inputElement.classList.add("form__input_type_error");
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add("form__input-error_active");
-  };
-
-  const hideInputError = (inputElement) => {
-    const errorElement = inputElement.nextElementSibling;
-    inputElement.classList.remove("form__input_type_error");
-    errorElement.classList.remove("form__input-error_active");
-    errorElement.textContent = "";
-  };
-
-  const isValid = (inputElement) => {
-    if (!inputElement.validity.valid) {
-      showInputError(inputElement, inputElement.validationMessage);
-    } else {
-      hideInputError(inputElement);
-    }
-  };
-
-  const hasInvalidInput = (inputList) => {
-    return inputList.some((inputElement) => !inputElement.validity.valid);
-  };
-
-  const toggleButtonState = (inputList, buttonElement) => {
-    if (hasInvalidInput(inputList)) {
-      buttonElement.classList.add("form__submit_inactive");
-      buttonElement.disabled = true;
-    } else {
-      buttonElement.classList.remove("form__submit_inactive");
-      buttonElement.disabled = false;
-    }
-  };
-
-  const setEventListeners = (formElement) => {
-    const inputList = Array.from(formElement.querySelectorAll(".form__input"));
-    const buttonElement = formElement.querySelector(".form__submit");
-
-    toggleButtonState(inputList, buttonElement);
-
-    inputList.forEach((inputElement) => {
-      inputElement.addEventListener("input", () => {
-        isValid(inputElement);
-        toggleButtonState(inputList, buttonElement);
-      });
-    });
-  };
-
-  enableValidation = () => {
-    const formList = Array.from(document.querySelectorAll(".form"));
-    formList.forEach((formElement) => {
-      formElement.addEventListener("submit", (evt) => {
-        evt.preventDefault();
-      });
-      setEventListeners(formElement);
-    });
-  };
-
-  enableValidation();
+const allForms = document.querySelectorAll(settingsObject.formSelector);
+allForms.forEach((formElement) => {
+  const formValidator = new FormValidator(settingsObject, formElement);
+  formValidator.enableValidation();
 });
