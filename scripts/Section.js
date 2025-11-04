35e373b92d7a04f02238d65a0b4decd9a46c8f62
yaelramirez11 <yaleramso@gmail.com>
2025-11-04 08:35:52 -0600

    Clase Section finalizada

---
+++ scripts/Section.js
@@ -0,0 +1,25 @@
+class Section {
+  //esta clase es para presentar una lista de elementos en una pagina web
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  addItem(element) {
+    this._container.append(element);
+  }
+
+  clear() {
+    //se limpia el contenedor del HTML interno
+    this._container.innerHTML = "";
+  }
+
+  renderItems() {
+    this.clear(); //antes de renderizar, se limpia el contenedor
+    this._items.forEach((item) => {
+      //para cada item del array items
+      this._renderer(item); //se llama a la función de callback en cada item, para renderizar a c/u
+    });
+  }
+}
